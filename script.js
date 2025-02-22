29f54e8ce93afe79db829a5d164782dfe055cbef
golinikhil12 <golinikhil12@gmail.com>
2025-02-22 09:55:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["On the path to becoming a Frontend Developer 🚀",],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop: true
+})
