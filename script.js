b4da8cf7ef117586cf1dd9597e2c4f11d6bd4bb3
golinikhil12 <golinikhil12@gmail.com>
2025-04-13 10:07:28 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["On the path to becoming a Frontend Developer 🚀",],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 500,
-    loop: true
-})
